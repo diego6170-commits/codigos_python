332313aff29cd5875945cbdb9241c36ec7cf3b05
Diego Cunha <diego6170@aluno.laboro.edu.br>
2025-10-15 19:44:46 -0300

    Trabalhando com tupla

---
+++ tupla.py
@@ -0,0 +1,15 @@
+# Criando uma tupla
+frutas = ("Banana","Uva","Morango","Açerola")
+
+#print(type(frutas))
+
+print(frutas)
+
+frutas[2] =  "Manga" # Aqui teremos um erro
+
+print(frutas[2])
+
+# Exibir todas as frutas
+
+for item in frutas:
+    print(item)
